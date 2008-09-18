b5aad5ed11d33821df4281730a94bb653f580aad
Jonathan Tang <jonathan.d.tang@gmail.com>
2008-09-18 13:43:51 -0400

    Initial commit

---
+++ jsdoc.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python
+
+# Copyright (c) 2007/08 by Jonathan Tang
+# 
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+# 
+# The above copyright notice and this permission notice shall be included in
+# all copies or substantial portions of the Software.
+# 
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+# THE SOFTWARE.
+
+"""
+Python library & command-line tool for performing a variety of build
+& deployment tasks of jQuery plugins.  
+
+This offers the following features:
+
+* List all dependencies of a plugin or plugins
+* Check for method name conflicts among a set of plugins
+* Extract metadata from doc comments
+* Generate documentation for a set of files.
+
+It depends on the existence of certain @tags in the documentation.  These are:
+
+* @module: Display name of the module
+* @author: Author's name
+* @version: Version number
+* @organization: Name of sponsoring organization, if any
+* @license: License type (BSD/MIT/GPL/LGPL/Artistic/etc.)
+* @dependency: Filename of parent plugin.  Multiple tags allowed
+"""
+
